9dd030d9e72facb0e484e3dda6a2a1c3f6e91d92
Andreas Kloeckner <andreas@grizzly.selfip.net>
2007-02-15 14:59:36 -0500

    Fine tube, inverse mesh generation. clean script. Split out SFish file parsing.

--- src/generate_mesh.py
+++ src/generate_mesh.py
@@ -1,103 +1,93 @@
-from meshpy.tet import MeshInfo, build
+from meshpy.tet import MeshInfo, build, \
+        generate_surface_of_revolution, \
+        EXT_CLOSED_IN_RZ, EXT_OPEN
 
 
 
 
-def parse_superfish_format(filename):
-    import re
+def bounding_box(tuples):
+    assert tuples
+
+    l = len(tuples[0])
+    return [(min(t[i] for t in tuples), max(t[i] for t in tuples))
+            for i in range(l)]
+
+
+
     
-    lines = [line.strip() for line in file(filename, "r").readlines() 
-            if line.strip().startswith("&po")]
-
-    # parse the file
-    line_re = re.compile(r"^\&po\s+(.*)\s*\&$")
-    key_val_re = re.compile(r"^\s*([0-9a-zA-Z]+)\s*\=\s*(.*)\s*$")
-    proplines = []
-    for line in lines:
-        line_match = line_re.match(line)
-        assert line_match
-        data = line_match.group(1).split(",")
-
-        properties = {}
-        for datum in data:
-            datum_match = key_val_re.match(datum.strip())
-            assert datum_match
-            properties[datum_match.group(1)] = float(datum_match.group(2))
-        proplines.append(properties)
-
-    # concoct x-y-points
-    from math import atan2, sin, cos, pi, ceil
-    import pylinear.array as num
-    import pylinear.computation as comp
-
-    def add_point(pt):
-        points.append((pt[1], pt[0]))
-
-    points = []
-    for i, p in enumerate(proplines):
-        if "nt" in p:
-            # draw arc
-            last_point = points[-1]
-            center = num.array((p["x0"], p["y0"]))
-            r = p["r"]
-            theta = pi/180.*p["theta"]
-
-            tangent = num.array((cos(theta), sin(theta)))
-            upward_normal = num.array((-sin(theta), cos(theta)))
-
-            #print 180*theta/pi, last_point, center, tangent, upward_normal, last_point-center
-            if (last_point - center)*upward_normal < 0:
-                # draw ccw (positive) arc / upward
-                phi_start = 3*pi/2 + theta
-                phi_end = phi_start + pi/2
-            else:
-                # draw cw (negative) arc / downward
-                phi_start = pi/2 + theta
-                phi_end = phi_start - pi/2
-            steps = 10
-            dphi = (phi_end-phi_start) / steps
-            phi = phi_start+dphi
-
-            for i in range(steps):
-                points.append(center + r*num.array((cos(phi), sin(phi))))
-                phi += dphi
-        else:
-            points.append(num.array((p["x"], p["y"])))
-
-    #outf = file("gun-lines.dat", "w")
-    #for p in points:
-        #outf.write("%f\t%f\n" % (p[0], p[1]))
-    #outf.close()
-
-    # turn (x,y) into (r,z)
-    return [(p[1], p[0]) for p in points]
-
-
-            
-
-
-
-
-
 def main():
-    simple_rz = [
-        (0,0),
-        (1,1),
-        (1,2),
-        (0,3),
-        ]
+    from superfish import parse_superfish_format
+
     mesh_info = MeshInfo()
-    points, facets = generate_surface_of_revolution(simple_rz)
+    rz = parse_superfish_format("gun.am", 0.3)
+
+    closure = EXT_OPEN
+
+    build_kwargs = {}
+
+    generate_inverse = False
+    if generate_inverse:
+        # chop off points with zero radius
+        while rz[0][0] == 0:
+            rz.pop(0)
+        while rz[-1][0] == 0:
+            rz.pop(-1)
+
+        # construct outer cylinder
+        ((min_r, max_r), (min_z, max_z)) = bounding_box(rz)
+        first_z = rz[0][1]
+        last_z = rz[-1][1]
+        rz.extend([
+                (max_r+2, last_z),
+                (max_r+2, first_z),
+                ])
+        closure=EXT_CLOSED_IN_RZ
+
+    generate_fine_tube = True
+    if generate_fine_tube:
+        # chop off points with zero radius
+        while rz[0][0] == 0:
+            rz.pop(0)
+        while rz[-1][0] == 0:
+            rz.pop(-1)
+
+        # construct outer cylinder
+        ((min_r, max_r), (min_z, max_z)) = bounding_box(rz)
+        first_z = rz[0][1]
+        last_z = rz[-1][1]
+        first_r = rz[0][0]
+        tube_r = first_r*0.6
+
+        rz.insert(0, (tube_r, min_z))
+        rz.append((tube_r, max_z))
+
+        points, facets = generate_surface_of_revolution(rz,
+                closure=closure)
+
+        tube_points, tube_facets = generate_surface_of_revolution(
+                [(0,min_z), (tube_r, min_z), (tube_r, max_z),
+                    (0, max_z)], point_idx_offset=len(points))
+
+        points.extend(tube_points)
+        facets.extend(tube_facets)
+
+        # set regional max. volume
+        mesh_info.regions.resize(1)
+        mesh_info.regions[0] = [0, 0,(max_z+min_z)/2, 0, 1e-4]
+
+        build_kwargs["varvolume"] = True
+    else:
+        points, facets = generate_surface_of_revolution(rz,
+                closure=closure)
+
 
     mesh_info.set_points(points)
     mesh_info.set_facets(facets, [0 for i in range(len(facets))])
-    #mesh_info.save_nodes("test")
-    #mesh_info.save_poly("test")
-    #mesh_info.load_poly("test")
-    mesh = build(mesh_info)
-    mesh.write_vtk("my_mesh.vtk")
-    #mesh.save_elements("gun")
-    #mesh.save_nodes("gun")
+    mesh_info.save_poly("gun")
+    mesh_info.save_nodes("gun")
+    mesh = build(mesh_info, **build_kwargs)
+    print "%d elements" % len(mesh.elements)
+    mesh.write_vtk("gun.vtk")
 
 
 
